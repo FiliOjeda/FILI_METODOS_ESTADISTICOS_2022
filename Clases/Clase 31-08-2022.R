2ae3bb5d92ce6925be6432a803fc9ab7d6bbdc2f
Fili <mo482616@gmail.com>
2022-09-19 09:05:18 -0500

    Update

--- Clases/Clase 31-08-2022.R
+++ Clases/Clase 31-08-2022.R
@@ -3,8 +3,8 @@
 # Fili lozoya ojeda
 # 21/08/2022
 
-Entrevistas <- read.csv("Clases/Encuesta_codificada.csv", header = T)
 
+Entrevistas <- read.csv("Clases/Encuesta_codificada.csv", header = T)
 
 
 # Entrevistadores ---------------------------------------------------------
@@ -37,4 +37,75 @@ Entrevistas$Fecha <- as.Date(Entrevistas$Fecha)
 
 gen <- table(Entrevistas$Genero)
 round(gen/length(Entrevistas$Genero)*100, 1)
-nfffbgb
+
+oi_4 <- table(Entrevistas$oi_4)
+prop.table(oi_4)*100
+
+-----------------------------------------
+  
+
+Entrevistas$oi_4a <- as.factor(Entrevistas$oi_4a)
+
+oi_4a <- table(Entrevistas$oi_4a)
+
+oi_4a
+
+
+prop.table(oi_4a)*100
+
+
+#CONVIVENCIA
+
+ef_1 <- table(Entrevistas$ef_1)
+ef_1
+prop.table(ef_1)*100
+
+
+ins_1 <- table(Entrevistas$ins_1)
+ins_1
+prop.table(ins_1)*100
+
+
+ap_1 <- table(Entrevistas$ap_1)
+ap_1
+prop.table(ap_1)*100
+
+
+ap_2 <- table(Entrevistas$ap_2)
+ap_2
+prop.table(ap_2)*100
+
+
+
+ap_3 <- table(Entrevistas$ap_3)
+ap_3
+prop.table(ap_3)*100
+
+
+#Guardar la BD con los datos nuevos en formato csv
+
+write.csv(Entrevistas, "Encuesta_codificada.csv")
+
+
+
+
+#Paleta de colores
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
